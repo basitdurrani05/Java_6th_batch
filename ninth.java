c9e8b2d93cbd6315455abcf02f3055c5e2da391b
basitdurrani <basitdurrani05786@gmail.com>
2025-06-17 21:27:40 +0530

    Updated

---
+++ ninth.java
@@ -0,0 +1,72 @@
+// Loops
+
+
+// 1). For
+// 2). While
+// 3). do-while
+// 4). for-each
+
+public class ninth {
+
+
+    public static void main(String[] args) {
+
+
+        // int i=1   : Start
+
+        // i<=10   : Kahan tak chalega
+
+        // i++    : Increament Part
+
+
+        // 1). For
+
+        // for(int i=1; i<=100; i++){
+
+        //     System.out.println("Hello World");
+        // }
+
+
+        // for(int i=10; i>=1; i--){
+
+        //     System.out.println(i);
+        // }
+
+
+
+        // 2). While
+
+
+        // int i=1;
+
+
+        // while(i>=10){
+
+
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+
+        // 3). do-while
+
+        // kam sae kam ek baar
+
+
+        // int i=1;
+
+        // do{
+
+        //     System.out.println("Hello World");
+        //     i++;
+        // }while(i>=10);
+
+
+
+        
+
+
+    }
+    
+}
