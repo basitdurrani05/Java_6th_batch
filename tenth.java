1be58f80cdaa025ab80149cfc004ee1a9c3a3ab1
basitdurrani <basitdurrani05786@gmail.com>
2025-06-18 21:27:21 +0530

    Updated

---
+++ tenth.java
@@ -0,0 +1,62 @@
+// Array
+// Collection of similar type data.
+
+// Two Types
+
+// 1).One Dimensional/1D
+
+// 2). Multi-dimensional/2D/3D/4D
+
+import java.util.*;
+
+public class tenth {
+
+    public static void main(String[] args) {
+
+        // One Dimensional
+
+        // int []a = {12,13,14,15,16,17,18,19,20,21,22,23};
+
+        // Index
+
+        // 12 - 0
+        // 13 - 1
+        // 14 - 2
+        // 15 - 3
+
+        // System.out.println(a);  address
+
+        // for(int i=0; i<a.length; i++){
+
+        //     System.out.println(a[i]);
+        // }
+
+
+        // Input
+
+
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter Array Elements : ");
+        // int []a = new int[5];
+
+
+        // Input
+
+        // for(int i=0; i<a.length; i++){
+
+        //     a[i] = sc.nextInt();
+        // }
+
+
+        // Output 
+
+        // System.out.println("Array Elements Output : ");
+        // for(int i=0; i<a.length; i++){
+
+        //     System.out.println(a[i]);
+        // }
+        
+    }
+    
+}
